aea9f1c1ae8a3fac46a64bb6d019e11c947cef07
MadMentality <madnesees@gmail.com>
2024-06-30 02:00:45 -0400

    Change web icon

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 import styles from "./Nav.module.scss";
 import { useState } from "react";
 import menu from "../../../public/static/images/Nav/menu-2.png";
-import logo from "../../../public/static/images/Nav/logo.jpg";
+import logo from "../../../public/static/images/Nav/logo-trans.png";
 import search from "../../../public/static/images/Nav/search.png";
 
 function Nav() {
